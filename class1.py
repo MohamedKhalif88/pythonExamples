9e84abdcb6eb773bf421915eb0a3b0195aa111c7
makfauzan <simg1881@gmail.com>
2019-02-27 08:55:07 +0300

    class1 edited

---
+++ class1.py
@@ -0,0 +1,12 @@
+class Awesome:
+
+    def the_first(self):
+        print("this is the first function in the class awesome")
+
+    def the_second(self):
+        print("and this is my second function in the class awesome")
+
+
+nice = Awesome()
+nice.the_first()
+nice.the_second()
