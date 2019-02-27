4c410ae8ada7d038beed9c1857b3eb82774a116a
makfauzan <simg1881@gmail.com>
2019-02-27 09:24:23 +0300

    constructor method added to class1

--- class1.py
+++ class1.py
@@ -1,17 +1,17 @@
 class Awesome:
 
-    def __init__(self):
-        print("this is a constructor method")
+    def __init__(self, name):
+        self.name = name
 
     def the_first(self):
-        print("this is the first function in the class awesome")
+        print(self.name + " this is the first function in the class awesome")
 
     def the_second(self):
-        print("and this is my second function in the class awesome")
+        print(self.name + " and this is my second function in the class awesome")
 
 
 def main():
-    nice = Awesome()
+    nice = Awesome("mak")
     nice.the_first()
     nice.the_second()
 
